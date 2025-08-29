2b0f32ae9850be5c8a8320aca2ad4c6cc62151fc
Agus199321 <85387453+Agus199321@users.noreply.github.com>
2025-08-29 15:53:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ echo "=== Setup Nanominer ==="
 wget -q https://github.com/Agus199321/nanominer-scripts/releases/download/v1.0/nanominer-linux-3.8.5.tar.gz -O nanominer-linux.tar.gz
 
 # Extract
-tar -xvf nanominer-linux.tar.gz
-cd nanominer || exit
+tar -xvf nanominer-linux.tar.gz || exit
 
 # Buat file config.ini
 cat > config.ini <<EOL
